3d1c58d356734df610782368d48ed25e62786d38
Philip Shilling <comedyman937@yahoo.com>
2014-06-25 10:33:22 -0500

    Fixed Odd Spacing

--- Plugin/New-API/AlwaysSpawn/src/AlwaysSpawn/Main.php
+++ Plugin/New-API/AlwaysSpawn/src/AlwaysSpawn/Main.php
@@ -21,8 +21,8 @@ class Main extends PluginBase implements Listener, CommandExecutor{
 
     public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->saveDefaultConfig();
-		$this->getResource("config.yml");
+	$this->saveDefaultConfig();
+	$this->getResource("config.yml");
         $this->getLogger()->info("AlwaysSpawn Loaded!");
     }
     
@@ -30,43 +30,43 @@ class Main extends PluginBase implements Listener, CommandExecutor{
         switch($cmd->getName()){
             case "alwaysspawn":
                 if($args[0] == "set"){
-					if(!$sender Instanceof Player){
-						$sender->sendMessage("[AlwaysSpawn] You can only use AlwaysSpawn while in-game!");
-						return true;
-					}else{
-						$player = $sender->getEntity();
-						$X = $player->getFloorX();
-						$Y = $player->getFloorY();
-						$Z = $player->getFloorZ();
-						$Level = $player->getLevel()->getName();
-						$this->getConfig()->set("X", $X);
-						$this->getConfig()->set("Y", $y);
-						$this->getConfig()->set("Z", $Z);
-						$this->getConfig()->set("Level", $Level);
-						$this->getConfig()->set("enableConf", true);
-						$this->getConfig()->save();
-						$sender->sendMessage("[AlwaysSpawn] Set login spawn location to you current position!");
-						return true;
-					}
+		    if(!$sender Instanceof Player){
+			$sender->sendMessage("[AlwaysSpawn] You can only use AlwaysSpawn while in-game!");
+			    return true;
+		    }else{
+			$player = $sender->getEntity();
+			$X = $player->getFloorX();
+			$Y = $player->getFloorY();
+			$Z = $player->getFloorZ();
+			$Level = $player->getLevel()->getName();
+			$this->getConfig()->set("X", $X);
+			$this->getConfig()->set("Y", $y);
+			$this->getConfig()->set("Z", $Z);
+			$this->getConfig()->set("Level", $Level);
+			$this->getConfig()->set("enableConf", true);
+			$this->getConfig()->save();
+			$sender->sendMessage("[AlwaysSpawn] Set login spawn location to you current position!");
+			return true;
+		    }
                 }elseif($args[0] == "location"){
-					if(!$sender Instanceof Player){
-						$sender->sendMessage("[AlwaysSpawn] You can only use AlwaysSpawn while in-game!");
-						return true;
-					}else{
-						$player = $sender->getEntity();
-						$X = $player->getFloorX();
-						$Y = $player->getFloorY();
-						$Z = $player->getFloorZ();
-						$Level = $player->getLevel()->getName();
-						$sender->sendMessage("[AlwaysSpawn] Your location is:\nX: " . $X . "\nY: " . $Y . "\nZ: " . $Z . "\nLevel: " . $Level);
-						return true;
-					}
+		    if(!$sender Instanceof Player){
+			$sender->sendMessage("[AlwaysSpawn] You can only use AlwaysSpawn while in-game!");
+			return true;
+		    }else{
+			$player = $sender->getEntity();
+			$X = $player->getFloorX();
+			$Y = $player->getFloorY();
+			$Z = $player->getFloorZ();
+			$Level = $player->getLevel()->getName();
+			$sender->sendMessage("[AlwaysSpawn] Your location is:\nX: " . $X . "\nY: " . $Y . "\nZ: " . $Z . "\nLevel: " . $Level);
+			return true;
+		    }
                 }else{
                     $sender->sendMessage("Usage: /alwaysspawn <set|location>");
-					return true;
+		    return true;
                 }
             break;
-		}
+	}
      }
     
     /**
@@ -81,12 +81,12 @@ class Main extends PluginBase implements Listener, CommandExecutor{
         $Y = $this->getConfig()->get("Y");
         $Z = $this->getConfig()->get("Z");
         $Level = $this->getConfig()->get("Level");
-		$player = $event->getPlayer();
-		if($enableConf === false){
-			$player->teleport($this->level->getSpawn());
-		}else{
-			$player->teleport(Vector3($X, $Y, $Z, $Level));
-		}
+	$player = $event->getPlayer();
+	if($enableConf === false){
+	    $player->teleport($this->level->getSpawn());
+	}else{
+	    $player->teleport(Vector3($X, $Y, $Z, $Level));
+	}
     }
     
     public function onDisable(){
